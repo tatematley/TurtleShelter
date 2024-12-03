e7364b26874b74f209afa9b10b41bc7872ebde4e
prestonpark26 <153689632+prestonpark26@users.noreply.github.com>
2024-12-03 10:29:04 -0700

    userManagement pages and routes

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const knex = require("knex")({
         user: process.env.RDS_USERNAME || "postgres",
         password: process.env.RDS_PASSWORD || "turtles",
         database: process.env.RDS_DB_NAME || "turtle_shelter",
-        port: process.env.RDS_PORT || 5003,
+        port: process.env.RDS_PORT || 5432,
         ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
     }
 }); 
@@ -90,5 +90,66 @@ app.post('/login', (req, res) => {
     }
     res.redirect("/internalLanding")
   });
+
+app.get('/userManagement', (req, res) => {
+    knex.select().from('users').then(myusers => {
+        res.render('userManagement', {users: myusers})
+    });
+});
+
+app.get('/editUser/:id', (req, res) => {
+    knex.select('user_id',
+                'user_first_name',
+                'user_last_name',
+                'user_email',
+                'user_position',
+                'user_start_year',
+                'zip',
+                'user_phone').from('users').where('user_id', req.params.id).then(myusers => {
+                    res.render('editUser', {user: myusers})
+                }).catch(err => {
+                    console.log(err);
+                    res.status(500).json({err});
+                });
+});
+
+app.post('/editUser', (req, res) => {
+    knex('users').where('user_id', parseInt(req.body.user_id).update({
+        user_first_name: req.body.user_first_name.toUpperCase(),
+        user_last_name: req.body.user_last_name.toUpperCase(),
+        user_email: req.body.user_email,
+        user_phone: req.body.user_phone,
+        user_position: req.body.user_position,
+        user_start_year: req.body.user_start_year,
+        zip: parseInt(req.body.zip)
+    })).then(myusers => {
+        res.redirect('/userManagement');
+    });
+});
+
+app.post('/deleteUser/:id', (req, res) => {
+    knex('users').where('user_id', req.body.user_id).del().then(myusers => {
+        res.redirect('/userManagement')
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({err});
+    });
+});
+
+app.get('/addUser', (req, res) => {
+    res.render('addUser');
+});
+
+app.post('/addUser', (req, res) => {
+    knex('users').insert({
+        user_first_name: req.body.user_first_name.toUpperCase(),
+        user_last_name: req.body.user_last_name.toUpperCase(),
+        user_email: req.body.user_email,
+        user_phone: req.body.user_phone,
+        user_position: req.body.user_position,
+        user_start_year: req.body.user_start_year,
+        zip: parseInt(req.body.zip)
+    })
+})
 
 app.listen(port, () => console.log("Express is listening"));
