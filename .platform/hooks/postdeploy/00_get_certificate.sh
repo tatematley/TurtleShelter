135aec28c20d697545e715964d84f1c652fe950c
lonni2020 <153124510+lonni2020@users.noreply.github.com>
2024-12-05 19:11:35 -0700

    this studff

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d turtle-shell.us-east-1.elasticbeanstalk.com/ --nginx --agree-tos --email loni2020@byu.edu
-sudo certbot -n -d tsp26.is404.net/ --nginx --agree-tos --email loni2020@byu.edu
+sudo certbot -n -d turtle-shell.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email loni2020@byu.edu
+sudo certbot -n -d tsp26.is404.net --nginx --agree-tos --email loni2020@byu.edu
